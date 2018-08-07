8190f3b9a181704c63f0d35377fe00a90c9c7e7e
Thomas Shockman <tom.shockman@gmail.com>
2018-08-07 18:15:26 -0400

    Fix

---
+++ src/app/sections/sections.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {SectionServiceClient} from '../services/section.service.client';
+import {Section, User} from '../app.types';
+import {UserServiceClient} from '../services/user.service.client';
+import {EnrollmentServiceClient} from '../services/enrollment.service.client';
+
+@Component({
+  selector: 'app-sections',
+  templateUrl: './sections.component.html',
+  styleUrls: ['./sections.component.css']
+})
+export class SectionsComponent implements OnInit {
+  sections: Array<Section> = null;
+  currentUser: User = null;
+
+  constructor(private sectionServiceClient: SectionServiceClient,
+              private activatedRoute: ActivatedRoute,
+              private router: Router,
+              private userServiceClient: UserServiceClient,
+              private enrollmentServiceClient: EnrollmentServiceClient) { }
+
+  ngOnInit() {
+    this.fetchData();
+  }
+
+  fetchData = () => {
+    this.sectionServiceClient.getSectionsForCourseId(this.activatedRoute.snapshot.params['cid'])
+      .then(sections => {
+        console.log(JSON.stringify(sections, null, 2));
+        this.sections = sections;
+      });
+    this.userServiceClient.getProfile()
+      .then(user => {
+        console.log(JSON.stringify(user, null, 2));
+        this.currentUser = user;
+      });
+  }
+
+  enroll(section) {
+    this.enrollmentServiceClient.enroll(this.currentUser, section)
+      .then(this.fetchData);
+  }
+
+  unenroll(section) {
+    this.enrollmentServiceClient.unenroll(this.currentUser, section)
+      .then(this.fetchData);
+  }
+
+
+  isEnrolled(section) {
+    return this.currentUser.sections.filter(s => s._id === section._id).length;
+  }
+}
