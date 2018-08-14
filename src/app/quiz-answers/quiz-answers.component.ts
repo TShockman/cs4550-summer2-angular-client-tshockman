abf1e72d8c5454163d04ac3666f9c5480d41a64e
Thomas Shockman <tom.shockman@gmail.com>
2018-08-14 14:28:39 -0400

    Support grading of quizzes and showing submission

--- src/app/quiz-answers/quiz-answers.component.ts
+++ src/app/quiz-answers/quiz-answers.component.ts
@@ -11,15 +11,61 @@ import {ActivatedRoute} from '@angular/router';
 export class QuizAnswersComponent implements OnInit {
 
   submission: Submission = null;
+  gradedAnswers: object[] = [];
+  earnedPoints = 0;
+  totalPoints = 0;
 
   constructor(private submissionServiceClient: SubmissionServiceClient,
               private activatedRoute: ActivatedRoute) { }
 
   fetchData = () => {
+    this.submission = null;
+    this.gradedAnswers = [];
+    this.earnedPoints = 0;
+    this.totalPoints = 0;
     const qid = this.activatedRoute.snapshot.params['qid'];
     const sid = this.activatedRoute.snapshot.params['sid'];
     this.submissionServiceClient.getSubmission(qid, sid)
-      .then(submission => this.submission = submission);
+      .then(submission => {
+        this.gradedAnswers = submission.answers.slice();
+        for (const gradedAnswer of this.gradedAnswers) {
+          this.totalPoints += gradedAnswer.question.points;
+          switch (gradedAnswer.question.questionType) {
+            case 'TRUE_FALSE':
+              if (gradedAnswer.tfAnswer === gradedAnswer.question.isTrue) {
+                gradedAnswer.correct = true;
+                this.earnedPoints += gradedAnswer.question.points;
+              }
+              break;
+            case 'CHOICE':
+              const correctChoice = gradedAnswer.question.choices.find(choice => choice.correct).text;
+              gradedAnswer.correctChoice = correctChoice;
+              if (correctChoice === gradedAnswer.mcAnswer) {
+                gradedAnswer.correct = true;
+                this.earnedPoints += gradedAnswer.question.points;
+              }
+              break;
+            case 'FILL_BLANKS':
+              let allTrue = true;
+              for (let i = 0; i < gradedAnswer.question.blanks.length; i++) {
+                console.log(gradedAnswer.question.blanks[i], '===', gradedAnswer.fitbAnswers[i])
+                allTrue = allTrue && gradedAnswer.question.blanks[i] === gradedAnswer.fitbAnswers[i];
+              }
+              if (allTrue) {
+                gradedAnswer.correct = true;
+                this.earnedPoints += gradedAnswer.question.points;
+              }
+              break;
+            case 'ESSAY':
+              if (gradedAnswer.essayAnswer.length > 50) {
+                gradedAnswer.correct = true;
+                this.earnedPoints += gradedAnswer.question.points;
+              }
+              break;
+          }
+        }
+        this.submission = submission;
+      });
   }
 
   ngOnInit() {
